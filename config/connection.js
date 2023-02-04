c7784909c573aece3ed5da26f09ffe7fc950481b
Jerry Wan <craftersol728@gmail.com>
2023-02-04 14:18:59 -0800

    finalzed

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,13 @@
 require('dotenv').config();
 
-const sequelize = require('sequelize');
+const Sequelize = require('sequelize');
 
 let sequelize;
 if (process.env.JAWSDB_URL) {
   sequelize = new Sequelize(process.env.JAWSDB_URL);
 } else {
 
-   sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+   sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
     dialect: process.env.DB_DIALECT,
